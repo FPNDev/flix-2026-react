6f739d4aeee6caf9a73963d8c833bbcd956369c0
fpn <fpndev@gmail.com>
2026-09-22 14:17:37 +0300

    fix: mobile toasts position and size

--- src/components/DesignSystem/Toast/ToastContainer.tsx
+++ src/components/DesignSystem/Toast/ToastContainer.tsx
@@ -2,11 +2,20 @@ import classes from './ToastContainer.module.scss';
 import { Toast } from './Toast';
 import { useToastStates } from './ToastContext';
 
-export function ToastContainer() {
+type Props = {
+  offset: string;
+};
+
+export function ToastContainer({ offset }: Props) {
   const toasts = useToastStates();
 
   return (
-    <div className={classes.toastStack}>
+    <div
+      className={classes.toastStack}
+      style={{
+        '--toast-block-offset': offset,
+      }}
+    >
       {toasts.map((toast) => (
         <Toast key={toast.id} toast={toast} />
       ))}
