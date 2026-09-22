3cfbdc56817d75ea6a383b414ad8a23843536713
fpn <fpndev@gmail.com>
2026-09-22 16:40:28 +0300

    fix: @ alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, 'src'),
+    },
+  },
   server: {
     host: process.env.NODE_ENV !== 'development' ? true : false,
   },
